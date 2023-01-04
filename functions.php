c8a5d08d2f040d80fd8a509e5243ed86b61ff1ac
diego.escobar <diego@diegoescobar.ca>
2023-01-04 18:21:37 -0500

    Darkmode Layout Updates

--- functions.php
+++ functions.php
@@ -2,6 +2,8 @@
 
 /**
  * Enqueue scripts and styles.
+ * 
+ * https://github.com/ForEvolve/bootstrap-dark
  */
 
 
@@ -26,12 +28,12 @@ function cleo_dark_scripts() {
 
 	wp_enqueue_style( 'cleo-style', get_template_directory_uri().'/style.css', array(), _S_VERSION );
 
-	wp_enqueue_style( 'bootstrap-dark', get_stylesheet_directory_uri() . '/bootstrap-dark/css/bootstrap-dark.css', array(), _S_VERSION );
+	// wp_enqueue_style( 'bootstrap-dark', get_stylesheet_directory_uri() . '/bootstrap-dark/css/bootstrap-dark.css', array(), _S_VERSION );
 	
 	wp_enqueue_style( 'cleo-dark-style', get_stylesheet_uri(), array(), _S_VERSION );
 
-	wp_style_add_data( 'cleo-dark-style', 'rtl', 'replace' );
+	// wp_style_add_data( 'cleo-dark-style', 'rtl', 'replace' );
 
-	wp_enqueue_style( 'cleo-layout-style', get_template_directory_uri().'/css/resume.css', array(), _S_VERSION );
+	// wp_enqueue_style( 'cleo-layout-style', get_template_directory_uri().'/css/resume.css', array(), _S_VERSION );
 }
 add_action( 'wp_enqueue_scripts', 'cleo_dark_scripts' );
---
+++ template-parts/content.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * Template part for displaying posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package cleo
+ */
+
+
+ $postClasses = (!is_single() ) ? 'col-6 col-lg-4' : '' ; 
+
+ $postClasses = ( is_active_sidebar('sidebar-widgets') || is_active_sidebar('archive-widgets')) ? 'col-12 col-3' : $postClasses;
+ 
+ ?>
+	 <!-- Page content-->
+		 <article id="post-<?php the_ID(); ?>" <?php post_class( $postClasses ); ?>>
+			<!-- Post header-->
+			<header class="entry-header mb-4">
+				<!-- Post title-->
+				<?php
+				if ( is_singular() ) :
+					the_title( '<h1 class="entry-title fw-bolder mb-0">', '</h1>' );
+				elseif ( is_home() || is_front_page()):
+					// the_title( '<p class="entry-title fw-bolder mb-0">', '</p>' );
+				else :
+					the_title( '<h2 class="entry-title text-primary fw-bolder mb-1"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+				endif;
+				?>
+				<?php
+				if ( 'post' === get_post_type() ) :
+					?>
+					<div class="text-muted fst-italic mb-2">
+						<?php
+						// cleo_posted_on();
+						// cleo_posted_by();
+						?>
+					</div><!-- .entry-meta -->
+				<?php endif; ?>
+
+				<!-- Post categories-->
+				<footer class="entry-footer">
+					<?php cleo_entry_footer(); ?>
+				</footer><!-- .entry-footer -->
+				<?php
+				wp_link_pages(
+					array(
+						'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'cleopress' ),
+						'after'  => '</div>',
+					)
+				);
+				?>
+			</header>
+			<!-- Preview image figure-->
+			<figure class="mb-4">
+				<?php cleo_post_thumbnail(); ?>
+			</figure>
+			<!-- Post content-->
+			<section class="mb-5">
+			<?php if ( is_singular() ) : 
+				the_content(
+					sprintf(
+						wp_kses(
+							/* translators: %s: Name of current post. Only visible to screen readers */
+							__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'cleopress' ),
+							array(
+								'span' => array(
+									'class' => array(),
+								),
+							)
+						),
+						wp_kses_post( get_the_title() )
+					)
+				);
+				else :
+					the_excerpt(sprintf(
+						wp_kses(
+							/* translators: %s: Name of current post. Only visible to screen readers */
+							__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'cleopress' ),
+							array(
+								'span' => array(
+									'class' => array(),
+								),
+							)
+						),
+						wp_kses_post( get_the_title() )
+					) ); 
+				endif; ?>
+			</section>
+			<footer class="entry-footer">
+				<?php cleo_entry_footer(); ?>
+			</footer><!-- .entry-footer -->	
+		</article>
+		<!-- #post-<?php the_ID(); ?> -->
+	<?php if ( !is_singular() ) : ?>
+	<hr class="m-0" />
+	<?php endif; ?>
+	
